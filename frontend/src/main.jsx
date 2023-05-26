67eebbc721dc7e88e06f1f9c728986014c20d565
Oscar Ossa <oskrlex17@gmail.com>
2023-05-26 09:31:23 -0500

    feat(app): add router and routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './style.css'
 import App from './App'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { HitDetails } from './components/HitDetails';
 
 const container = document.getElementById('root')
 
@@ -9,6 +11,13 @@ const root = createRoot(container)
 
 root.render(
     <React.StrictMode>
-        <App/>
+        <HashRouter basename={"/"}>
+            {/* The rest of your app goes here */}
+            <Routes>
+                <Route path="/" element={<App />} exact />
+                <Route path="/hit-details" element={<HitDetails />} exact />
+                {/* more... */}
+            </Routes>
+        </HashRouter>,
     </React.StrictMode>
 )
