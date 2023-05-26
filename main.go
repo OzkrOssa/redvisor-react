5c0aeb72ec5df5929783b7e4108c72cdca1b058b
Oscar Ossa <oskrlex17@gmail.com>
2023-05-26 09:22:32 -0500

    chore(app): change width &  minWidth

--- main.go
+++ main.go
@@ -18,16 +18,17 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "redvisor-react",
-		Width:  1024,
+		Width:  1366,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		// BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 		},
+		MinWidth: 1366,
 	})
 
 	if err != nil {
